05063d1ca6226d4e439868d5d9d60d47895d999c
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2023-10-26 23:18:21 -0400

    agregando funcionalidad en la plantilla a seguir

--- ConceptosBasicos/firstCard.js
+++ ConceptosBasicos/firstCard.js
@@ -1,26 +1,27 @@
-import { cambiarColor, scrollNavegacion } from "../funcionesGenerales/funcionalidades.js";
+import { cambiarColor, checkLeido, scrollNavegacion } from "../funcionesGenerales/funcionalidades.js";
 
 const btnNav = document.querySelectorAll('.btn-nav');
 btnNav.forEach(btn => {
     btn.addEventListener('click', e => {
         const IDELEMENT = e.target.id;
+        cambiarColor(btnNav, e.target);
         switch (IDELEMENT) {
             case '1':
-                scrollNavegacion(110);
-                cambiarColor(btn);
+                scrollNavegacion(0);
                 break;
             case '2':
-                scrollNavegacion(940);
-                cambiarColor(btn);
+                scrollNavegacion(830);
                 break;
             case '3':
-                scrollNavegacion(1800);
-                cambiarColor(btn);
+                scrollNavegacion(1700);
                 break;
             case '4':
                 scrollNavegacion(2000);
-                cambiarColor(btn);
                 break;
         }
     })
 })
+
+const check = document.querySelector('.check');
+const text = document.querySelector('.text');
+checkLeido(check, 'check1', 1, text);
--- funcionesGenerales/funcionalidades.js
+++ funcionesGenerales/funcionalidades.js
@@ -3,9 +3,30 @@ export const scrollNavegacion = (cantidadScroll) => {
     document.documentElement.scrollTo(0, cantidadScroll);
 }
 
-//cambiar de color del menu de navegacion
-export const cambiarColor = (nodoElemento)=>{
-    nodoElemento.className = 'text-primary';
+export const cambiarColor = (btnNav, nodoActual) => {
+    btnNav.forEach(btn => {
+        btn.classList.remove('text-info');
+        btn.classList.add('text-white');
+    });
+    nodoActual.classList.remove('text-white');
+    nodoActual.classList.add('text-info');
 }
 
+export const checkLeido = (botonCheck, llaveLocalstorage, valorLocalStorage, cambiarTexto)=>{
+    let contadorClicks = 0;
+    botonCheck.addEventListener('click', e=>{
+        e.target.classList.remove('text-danger');
+        e.target.classList.add('text-success');
+        cambiarTexto.innerHTML = 'Ok... Sigue asi crack'
+        localStorage.setItem(llaveLocalstorage, valorLocalStorage);
+        contadorClicks++;
 
+        if(contadorClicks === 2){
+            e.target.classList.remove('text-success');
+            e.target.classList.add('text-danger');
+            cambiarTexto.innerHTML = 'Marca si completaste la lección'
+            localStorage.removeItem(llaveLocalstorage);
+            contadorClicks = 0;
+        }
+    })
+}
