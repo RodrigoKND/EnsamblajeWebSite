f530928990e8a59fccdd2ef7866d8000696a7c22
rodriknd <rodrigopacehco965@gmail.com>
2023-11-14 00:00:15 -0400

    agregando contenido en la quinta y sexta tarjeta

---
+++ jordan/cinco/cinco.js
@@ -0,0 +1,5 @@
+import {checkLeido} from '../../funcionesGenerales/funcionalidades.js';
+
+const check = document.querySelector('.check');
+const text = document.querySelector('.text');
+checkLeido(check, 'check5', 5, text, 'Sigue asi', 'Completa antes la lección');
---
+++ jordan/seis/seis.js
@@ -0,0 +1,5 @@
+import { checkLeido } from '../../funcionesGenerales/funcionalidades.js';
+
+const check = document.querySelector('.check');
+const text = document.querySelector('.text');
+checkLeido(check, 'check6', 6, text, 'Sigue asi', 'Completa antes la lección');
